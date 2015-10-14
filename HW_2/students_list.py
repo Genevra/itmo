abcbe1a74e32b61ea308d396b37e52f8d3746f40
elsoaprioris <elsoaprioris@gmail.com>
2015-10-14 14:36:10 +0300

    Add students_list.py

---
+++ HW_2/students_list.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3.4
+#coding: utf-8
+
+'''
+№2
+
+Делаем программу "students_list.py".
+
+1. Она должна сделать следующее:
+Создаем список.
+Заполняем его нашими именами (+фамилиями).
+
+2. Выводим имя одного студента на экран:
+Получаем индекс через функцию input().
+Выводим на экран студента по этому индексу.
+
+3. Выводим на экран имена нескольких студентов:
+Получаем через input() начало и конец среза.
+Выводим на экран студентов из такого среза.
+
+4. Находим количество студентов, в именах которых есть буква "р".
+
+5. Находим группы студентов с одинаковыми именами и создаем списки этих групп.
+'''
+
+#1
+students_list = ['Bukowski', 'Rasputin', 'Remark', 'Gorky', 'Pikul', 'Kuprin']
+
+#2
+student_index = int(input('Input Index Student: '))
+print('Students: ', students_list[student_index],'\n')
+
+#3
+student_slice_1 = int(input('Input first argumet for slice: '))
+student_slice_2 = int(input('Input second argumet for slice: '))
+print('Students slice:', students_list[student_slice_1:student_slice_2], '\n')
+
+#4
+count = 0
+for i in students_list:
+
+    if 'p' in i:
+        count += 1
+print("Students with 'p': ", count)
+
+
